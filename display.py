22a4fdafb96608dc5e83e136b31d720daadca2a8
Chris Yoon <chris7yoon@gmail.com>
2022-12-26 21:09:15 -0800

    food detection implemented

--- display.py
+++ display.py
@@ -7,6 +7,8 @@ class Display:
     BLUE = (0,0,255)
     BLACK = (0,0,0)
     SQUARE_PIX = 10
+    """For left, top corner as reference pt"""
+    DIRS = [[0,0], [SQUARE_PIX,0], [SQUARE_PIX, SQUARE_PIX], [0, SQUARE_PIX]]
     def __init__(self, width, height) -> None:
         """Initializes a Display object.
 
@@ -17,19 +19,46 @@ class Display:
         pygame.init()
         self.width = width
         self.height = height
-        self.dis = pygame.display.set_mode((width,height), flags=pygame.RESIZABLE)
+        self.dis = pygame.display.set_mode((width,height))
         pygame.display.set_caption("Snake Game")
         self.blue_rect = Rect(width//2, height//2, Display.SQUARE_PIX, Display.SQUARE_PIX)
         pygame.draw.rect(self.dis, Display.BLUE, self.blue_rect)
         pygame.display.update()
-        red_l,red_t = self.rand_red_location()
+        red_l,red_t = self.rand_food_location()
         self.red_rect = Rect(red_l, red_t, Display.SQUARE_PIX, Display.SQUARE_PIX)
         pygame.draw.rect(self.dis, Display.RED, self.red_rect)
         pygame.display.update()
         self.fps_millis = 5
 
-    def rand_red_location(self) -> tuple[int,int]:
-        """Returns a valid random location for the red rectangle to spawn.
+    def is_within_window(self, x, y) -> bool:
+        """Determines if the coordinates are within the defined window size
+
+        Args:
+            x (int): x coordinate of the pixel
+            y (int): y coordinate of the pixel
+
+        Returns:
+            bool: True if both x,y coordinates are within the window size
+        """        
+        return x >= 0 and x < self.width and y >= 0 and y < self.height
+
+    def has_eaten_food(self) -> bool:
+        """Determines whether or not the snake has eaten food.
+
+        Returns:
+            bool: True if the snake is at the food's location at the time of the method call
+        """
+        for dir in Display.DIRS:
+            x = self.red_rect.left + dir[0]
+            y = self.red_rect.top + dir[1]
+            if not self.is_within_window(x,y):
+                continue
+            if self.dis.get_at((x,y)) == pygame.Color(Display.BLUE):
+                return True
+        return False
+
+    def rand_food_location(self) -> tuple[int,int]:
+        """Returns a valid random location for the red rectangle (food) to spawn.
 
         Returns:
             tuple[int,int]: x,y coordinates of the top left part of the rectangle.
@@ -40,14 +69,14 @@ class Display:
         while not valid_loc:
             red_l = random.randint(0, self.width-Display.SQUARE_PIX)
             red_t = random.randint(0, self.height-Display.SQUARE_PIX)
-            dirs = [[0,0], [Display.SQUARE_PIX,0], [Display.SQUARE_PIX, Display.SQUARE_PIX], [0, Display.SQUARE_PIX]]
             # all dims of red square
             valid_loc = True
-            for dir in dirs:
+            for dir in Display.DIRS:
                 x = red_l + dir[0]
                 y = red_t + dir[1]
                 print(self.dis.get_at((x,y)))
-                if x < 0 or x > self.width or y < 0 or y > self.height or self.dis.get_at((x,y)) == pygame.Color(Display.BLUE):
+                # red square overwritten when blue moves on top
+                if not self.is_within_window(x,y) or self.dis.get_at((x,y)) == pygame.Color(Display.BLUE):
                     valid_loc = False
                     break
             print(x,y)
@@ -61,7 +90,7 @@ class Display:
         while not game_over:
             clk.tick(self.fps_millis)
             for event in pygame.event.get():
-                print(event)
+                # print(event)
                 if event.type == pygame.QUIT:
                     game_over = True
                 if event.type == pygame.KEYDOWN:
@@ -81,12 +110,14 @@ class Display:
                 else:
                     moved = False
             if not moved:
-                print(prev_move)
                 self.blue_rect.move_ip(prev_move[0], prev_move[1])
             self.dis.fill(Display.BLACK)
             pygame.draw.rect(self.dis, Display.RED, self.red_rect)
             pygame.draw.rect(self.dis, Display.BLUE, self.blue_rect)
             pygame.display.update()
+            print(self.dis.get_at((self.red_rect.left, self.red_rect.top)))
+            if self.has_eaten_food():
+                print("Food obtained")
             if self.blue_rect.left < 0 or self.blue_rect.right > self.width or self.blue_rect.top < 0 or self.blue_rect.bottom > self.height:
                 game_over = True
         print("Goodbye")
